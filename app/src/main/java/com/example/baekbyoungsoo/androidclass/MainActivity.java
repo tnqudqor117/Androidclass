eef78c59950606dd4ee0150e61a3701a7aa21408
BaekByoungSoo <tnqudqor1@naver.com>
2017-09-30 17:24:05 +0900

    Initial commit

--- app/src/main/java/com/example/baekbyoungsoo/androidclass/MainActivity.java
+++ app/src/main/java/com/example/baekbyoungsoo/androidclass/MainActivity.java
@@ -1,19 +1,19 @@
 package com.example.baekbyoungsoo.androidclass;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        Button btn = (Button)findViewById(R.id.button1);
+       Button btn = (Button)findViewById(R.id.button1);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -22,9 +22,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-   /* public void OnClickB1(View v) {
+   public void OnClickB1(View v) {
         Toast.makeText(getApplicationContext(), R.string.button_clicked_msg,
                 Toast.LENGTH_SHORT).show();
     }
-    */
+
 }
